2951d6a10f270d0a0c480af81c7c77bb64738e37
Mohasin Ahamed <mohsin.ahamed@hotmail.com>
2020-03-09 23:55:30 +0530

    last 1 problems from array remaining

--- Arrays/Rotate Image.py
+++ Arrays/Rotate Image.py
@@ -41,3 +41,24 @@ rotate the input matrix in-place such that it becomes:
 ]
 
 """
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        #get transpose
+        print(matrix)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if(i<=j):
+                    matrix[i][j] ,matrix[j][i]=   matrix[j][i],matrix[i][j]
+                    
+        print(matrix)
+        
+        # reverse each row
+        for i in range(len(matrix)):
+            matrix[i]= matrix[i][::-1]
+        
+        print(matrix)
